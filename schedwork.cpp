46e4157f343ebcea07eaf3e1ade1eeb75b768fbe
Blake Zhang <bdzhang@usc.edu>
2025-05-01 05:01:21 +0000

    done with initial coding

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool assignShifts( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, 
+  DailySchedule& sched, vector<size_t>& shiftCount, size_t dayIndex, size_t workerSlot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +40,49 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
 
+    sched.resize(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
 
+    vector<size_t> shiftCount(numWorkers, 0);
 
+    return assignShifts(avail, dailyNeed, maxShifts, sched, shiftCount, 0, 0);
 }
+
+bool assignShifts( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, 
+  DailySchedule& sched, vector<size_t>& shiftCount, size_t dayIndex, size_t workerSlot){
+
+    //base case
+    //schedule is fully build
+    if (dayIndex == sched.size()){
+      return true;
+    }
+
+    size_t numWorkers = avail[0].size();
+
+    //check each worker
+    for (Worker_T w = 0; w < numWorkers; w++){
+      if (avail[dayIndex][w] && shiftCount[w] < maxShifts && find(sched[dayIndex].begin(), sched[dayIndex].end(), w) == sched[dayIndex].end()){
+        sched[dayIndex][workerSlot] = w;
+        shiftCount[w]++;
+
+        //move to next day or slot ig
+        if (workerSlot+1 == dailyNeed){
+          if (assignShifts(avail, dailyNeed, maxShifts, sched, shiftCount, dayIndex+1, 0)){
+            return true;
+          }
+        }
+
+      }
+      //backtrack
+      sched[dayIndex][workerSlot] = INVALID_ID;
+      shiftCount[w]--;
+      
+    }
+
+    //no valid assignment found
+    return false; 
+}
+  
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void generateWords(string partialWord, string floatingLetters, const set<string>& dictionary, set<string>& validWords, int pos);
 
+//note: returns list of all possible words that could work
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    //DONE
+    set<string> validWords;
+    generateWords(in, floating, dict, validWords, 0);
+    return validWords; 
 
 }
 
 // Define any helper functions here
+
+void generateWords(string partialWord, string floatingLetters, const set<string>& dictionary, set<string>& validWords, int position){
+  //base case
+  //if word is fully filled then stop
+  if (position == partialWord.length()){
+    if (floatingLetters.empty() && dictionary.find(partialWord) != dictionary.end()){ //word exsts in dict
+      validWords.insert(partialWord);
+    }
+    return;
+  }
+
+  if (partialWord[position] != '-'){
+    generateWords(partialWord, floatingLetters, dictionary, validWords, position + 1); //RECURSE
+
+  }
+  else {
+    for (char ch = 'a'; ch <= 'z'; ch++){
+      partialWord[position] = ch;
+
+      size_t floatingIndex = floatingLetters.find(ch);
+      if (floatingIndex != string::npos){
+        string updatedFloating = floatingLetters;
+        updatedFloating.erase(floatingIndex, 1);
+        generateWords(partialWord, updatedFloating, dictionary, validWords, position+1); //RECURSE
+
+      }
+      else {
+        int openSpots = 0;
+        for (int i = position+1; i < partialWord.size(); i++){
+          if (partialWord[i] == '-'){
+            openSpots++;
+          }
+          if (openSpots >= floatingLetters.size()){
+            generateWords(partialWord, floatingLetters, dictionary, validWords, position+1); //RECURSE
+
+          }
+        }
+      }
+    }
+  }
+}
